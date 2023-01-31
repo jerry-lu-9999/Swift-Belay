f0b4b28fb8e07c973ec0f53feb97e140c4d3d8c4
jerry-lu-9999 <ljh274163788@gmail.com>
2023-01-30 20:29:06 -0800

    Initial Commit

---
+++ Swift Belay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Belay
+//
+//  Created by Jiahao Lu on 1/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
