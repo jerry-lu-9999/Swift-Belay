c8d018df25494465e99d90bef72b849fcfd3a2e0
jerry-lu-9999 <ljh274163788@gmail.com>
2023-02-06 21:06:58 -0800

    removed main storyboard, organized folders, added NavBar

---
+++ Swift Belay/Controllers/SBChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SBChatViewController.swift
+//  Swift Belay
+//
+//  Created by Jiahao Lu on 2/3/23.
+//
+
+import UIKit
+
+class SBChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Chat"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift Belay/Controllers/SBTabBarController.swift
@@ -0,0 +1,53 @@
+//
+//  SBTabBarController.swift
+//  Swift Belay
+//
+//  Created by Jiahao Lu on 1/30/23.
+//
+
+import UIKit
+
+/// Controller for the root tab controller and chat, profile controller
+class SBTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        setUpThreeTabs()
+    }
+    
+    private func setUpThreeTabs(){
+        
+        let homeVC = SBHomeViewController()
+        let chatVC = SBChatViewController()
+        let profileVC = SBProfileViewController()
+        
+        homeVC.navigationItem.largeTitleDisplayMode = .automatic
+        chatVC.navigationItem.largeTitleDisplayMode = .automatic
+        profileVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        /*
+         turn viewController into NavigationController for easier access
+         https://stackoverflow.com/questions/28751457/difference-between-navigation-controller-and-viewcontroller#:~:text=Navigation%20Controller%20consists%20of%20navigation,represents%20a%20single%20screen%20view.
+         */
+        
+        let nav1 = UINavigationController(rootViewController: homeVC)
+        let nav2 = UINavigationController(rootViewController: chatVC)
+        let nav3 = UINavigationController(rootViewController: profileVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Chat", image: UIImage(systemName: "paperplane"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 3)
+        
+        for nav in [nav1, nav2, nav3] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers([nav1, nav2, nav3], animated: true)
+    }
+
+
+    
+    
+}
+
