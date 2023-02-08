d459201035e2fa1e1ba2ce09e84c7fb51fef5891
billyihang <billyihang2002@gmail.com>
2023-02-07 20:10:07 -0800

    start of settings

---
+++ Swift Belay/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Swift Belay
+//
+//  Created by William Yang on 2/6/23.
+//
+
+import Foundation
---
+++ Swift Belay/Settings/SettingsPageView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsPageView.swift
+//  Swift Belay
+//
+//  Created by William Yang on 2/6/23.
+//
+
+import SwiftUI
+
+struct SettingsPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsPageView()
+    }
+}
